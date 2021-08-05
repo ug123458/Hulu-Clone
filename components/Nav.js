38b98909fbbff627c4c15741872eea78f22bb7d3
ug123458 <ug123458@gmail.com>
2021-08-05 19:23:11 +0530

    finished hulu frontend build

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,8 @@
 import requests from '../utils/requests'
-
+import { useRouter } from 'next/router'
 const Nav = () => {
+  const router = useRouter()
+
   return (
     <nav className='relative'>
       <div
@@ -10,6 +12,9 @@ const Nav = () => {
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
+            onClick={() => {
+              router.push(`/?genre=${key}`)
+            }}
             className=' last:pr-24 cursor-pointer transition duration-100
              transform hover:scale-125 hover:text-white active:text-red-500'
           >
